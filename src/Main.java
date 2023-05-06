180c3faaf67202d957467266a199998c4020af30
marrakechy <belmarrakechy21@coe.edu>
2023-05-05 22:33:54 -0500

    Second Commit

--- src/Main.java
+++ src/Main.java
@@ -3,28 +3,44 @@ import java.util.List;
 
 // Main class for testing
 
+
 public class Main {
     public static void main(String[] args) {
-        // create some individual items
-        OrderItem burger = new Product("Hamburger", 3.99);
-        OrderItem fries = new Product("French Fries", 1.99);
-        OrderItem pop = new Product("Soda Pop", 0.99);
-        // create a combo of individual items
+        // create some individual items and a combo of items
+        Product hamburger = new Product("Hamburger", 2.50);
+        Product fries = new Product("Fries", 1.50);
+        Product drink = new Product("Drink", 1.00);
         List<OrderItem> comboItems = new ArrayList<>();
-        comboItems.add(burger);
+        comboItems.add(hamburger);
         comboItems.add(fries);
-        comboItems.add(pop);
-        OrderItem combo1 = new Combo("Combo #1", comboItems);
-        // create an order and add items
+        comboItems.add(drink);
+        Combo combo = new Combo("Combo #1", comboItems);
+
+        // create an order and add items to it
         Order order = new Order();
-        order.addItem(burger);
-        order.addItem(combo1);
-        order.addItem(pop);
-        order.displayOrder();
-        System.out.printf("Total price: $%.2f\n", order.getTotalPrice());
-        // remove an item and display updated order
-        order.removeItem(1);
-        order.displayOrder();
-        System.out.printf("Total price: $%.2f\n", order.getTotalPrice());
+        order.addItem(hamburger);
+        order.addItem(fries);
+        order.addItem(drink);
+        order.addItem(combo);
+
+        // add more items to the order
+        Product chickenSandwich = new Product("Chicken Sandwich", 3.00);
+        order.addItem(chickenSandwich);
+
+        List<OrderItem> comboItems2 = new ArrayList<>();
+        comboItems2.add(hamburger);
+        comboItems2.add(drink);
+        Combo combo2 = new Combo("Combo #2", comboItems2);
+        order.addItem(combo2);
+
+        // display the order and get the total price
+        order.display();
+        System.out.println("Total price: $" + order.getTotalPrice());
+
+        // remove an item from the order and display the updated order and total price
+        order.removeItem(fries);
+        System.out.println("Updated order:");
+        order.display();
+        System.out.println("Updated total price: $" + order.getTotalPrice());
     }
 }
--- src/Order.java
+++ src/Order.java
@@ -2,7 +2,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 // Order class for managing a customer's order
-class Order {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Order {
     private List<OrderItem> items;
 
     public Order() {
@@ -13,22 +16,23 @@ class Order {
         items.add(item);
     }
 
-    public void removeItem(int index) {
-        items.remove(index);
-    }
-
-    public void displayOrder() {
-        System.out.println("Order items:");
-        for (int i = 0; i < items.size(); i++) {
-            System.out.printf("%d. %s - $%.2f\n", i+1, items.get(i).getName(), items.get(i).getPrice());
-        }
+    public void removeItem(OrderItem item) {
+        items.remove(item);
     }
 
     public double getTotalPrice() {
-        double totalPrice = 0;
+        double totalPrice = 0.0;
         for (OrderItem item : items) {
             totalPrice += item.getPrice();
         }
         return totalPrice;
     }
+
+    public void display() {
+        System.out.println("Order:");
+        for (OrderItem item : items) {
+            System.out.println("- " + item.getName() + ": $" + item.getPrice());
+        }
+    }
 }
+
